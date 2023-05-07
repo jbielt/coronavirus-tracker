7877819ff1aebb662837ada4283eb22d30460ac9
jbiel <jbielturon@gmail.com>
2023-05-07 17:20:17 +0200

    adding scheduled execution on request and parsing csv data

--- src/main/java/com/pim/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/pim/coronavirustracker/services/CoronaVirusDataService.java
@@ -3,6 +3,7 @@ package com.pim.coronavirustracker.services;
 import jakarta.annotation.PostConstruct;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -20,6 +21,7 @@ public class CoronaVirusDataService {
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     @PostConstruct//annotation method that executes on app start
+    @Scheduled(cron = "* * 1 * * *") //specified execute the first hour of every day (once every day)
     public void fetchVirusData() throws IOException, InterruptedException {
         //make the request
         HttpClient client = HttpClient.newHttpClient();
@@ -40,10 +42,10 @@ public class CoronaVirusDataService {
                 .build()
                 .parse(csvBodyReader);
 
+        //looping the records after the parse
         for (CSVRecord record : records) {
-            String id = record.get("ID");
-            String customerNo = record.get("CustomerNo");
-            String name = record.get("Name");
+            String state = record.get("Province/State");
+            System.out.println(state);
         }
     }
 
